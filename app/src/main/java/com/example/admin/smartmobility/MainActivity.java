9a4e8f55c4f6922d78f7c34978eaa79dab8e5fe5
Tran Anh Dat <trananhdat@gmail.com>
2018-07-03 23:57:04 +0700

    Cấu hình font trong file MainActivity.java

--- app/src/main/java/com/example/admin/smartmobility/MainActivity.java
+++ app/src/main/java/com/example/admin/smartmobility/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.admin.smartmobility;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Nhan Ctr+O la ra!
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Sau khi setContentView
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                            .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                            .setFontAttrId(R.attr.fontPath)
+                                            .build());
         setContentView(R.layout.activity_main);
     }
 }
